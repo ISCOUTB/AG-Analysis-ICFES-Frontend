9436abfcafc6f11814f3d4b140abf9c4f2968b98
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-09-27 14:34:55 -0500

    Minor change to the links and stuff

--- components/navbar/NavbarLinks.vue
+++ components/navbar/NavbarLinks.vue
@@ -23,8 +23,52 @@
 
 <template>
     <nav class="flex items-center gap-4 sm:gap-6">
-        <NuxtLink v-for="item in items" :key="item.label" :to="item.to">
+        <NuxtLink class="hidden sm:flex items-center" to="/">
+            <Icon
+                name="material-symbols-light:home-rounded"
+                class="text-xl font-medium text-gray-950"
+            />
+        </NuxtLink>
+
+        <NuxtLink
+            v-for="item in items"
+            :key="item.label"
+            :to="item.to"
+            class="hidden sm:flex"
+        >
             {{ capitalize(item.label) }}
         </NuxtLink>
+
+        <DropdownMenu>
+            <DropdownMenuTrigger class="sm:hidden">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-8 h-8"
+                >
+                    <rect width="7" height="7" x="3" y="3" rx="1" />
+                    <rect width="7" height="7" x="14" y="3" rx="1" />
+                    <rect width="7" height="7" x="14" y="14" rx="1" />
+                    <rect width="7" height="7" x="3" y="14" rx="1" />
+                </svg>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                    <NuxtLink to="/"> Home </NuxtLink>
+                </DropdownMenuItem>
+                <DropdownMenuItem v-for="item in items" :key="item.label">
+                    <NuxtLink :to="item.to" class="w-full h-full">
+                        {{ capitalize(item.label) }}
+                    </NuxtLink>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     </nav>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,10 @@
     <section :key="$route.fullPath">
         <HomeAnalysisOptions />
         <HomeAditionalOptions />
+        <div class="w-full bg-background py-12">
+            <div class="container px-4 md:px-6">
+                <Button> Submit </Button>
+            </div>
+        </div>
     </section>
 </template>
