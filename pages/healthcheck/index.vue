a4945fb06de22b5b9f8565c30086cfea465d403e
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-09-27 14:45:40 -0500

    Creating the page healthcheck just to see the status of the site

---
+++ pages/healthcheck/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    definePageMeta({
+        layout: false,
+    });
+
+    const { data: departments } = useAsyncGql({
+        operation: "departments",
+    });
+</script>
+
+<template>
+    <section :key="$route.fullPath" class="w-full bg-background py-12">
+        <div class="container px-4 md:px-6 flex flex-col gap-4">
+            <div class="space-x-4">
+                <span class="font-bold text-2xl">Departments</span>
+                <span
+                    v-if="departments"
+                    class="text-green-500 font-bold px-4 py-2 border border-green-500 rounded-md"
+                >
+                    Active
+                </span>
+                <span
+                    v-else
+                    class="text-rose-500 font-bold px-4 py-2 border border-green-500 rounded-md"
+                >
+                    Not Active
+                </span>
+            </div>
+        </div>
+    </section>
+</template>
